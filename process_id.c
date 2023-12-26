cf6928dba3ac14430ba3f8b212aec6fb58b99611
Eric Bjarstal <ericbmousy@gmail.com>
2023-12-26 17:39:23 +0100

    add program qui teste getpid, getppid, wait, sleep, fork...

--- process_id.c
+++ process_id.c
@@ -3,5 +3,21 @@
 #include <unistd.h>
 
 int main(void) {
+    int id = fork();
+
+    if (id == 0) {
+        sleep(1);
+    }
+
+    printf("ID actuel: %d\nID du ps père: %d\n", getpid(), getppid());
+
+    int res = wait(NULL);
+    if (res == -1) {
+        printf("Pas de ps fils en attente\n");
+    }
+    else {
+        printf("Fin d'exécution de %d\n", res);
+    }
+
     return 0;
 }
