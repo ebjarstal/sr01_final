32a8b5a99e580176b8f7541d97e205b4472b667c
Eric Bjarstal <ericbmousy@gmail.com>
2023-12-26 16:13:04 +0100

    create wait.c

--- wait.c
+++ wait.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]) {
+
+int main(void) {
+    int id = fork();
+    int n;
+
+    if (id != 0) {  // si c'est le ps père
+        n = 6;
+        wait(&id);  // on attend que le ps fils termine d'afficher ses nombres
+    }
+    else {  // si c'est le ps fils
+        n = 1;
+    }
+
+    int i;
+    for (i = n; i < n+5; i++) {
+        printf("%d ", i);
+        fflush(stdout);
+    }
+    printf("\n");
+
     return 0;
 }
