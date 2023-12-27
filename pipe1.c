18fc2ec3091b5ad699e46add11bc50c62f9aa0f4
Eric Bjarstal <ericbmousy@gmail.com>
2023-12-27 17:27:42 +0100

    add code

---
+++ pipe1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+int main(void) {
+    int fd[2];
+    /* fd[0] - lecture  */
+    /* fd[1] - écriture */
+
+    if (pipe(fd) == -1) {
+        return 1;
+    }
+    /* la fonction pipe met les "clés"   */
+    /* du pipe dans fd (file descriptor) */
+
+    int id = fork();
+
+    if (id == -1) {
+        return 2;
+    }
+    else if (id == 0) {
+        /* code du processus fils */
+
+        close(fd[0]);
+        /* on ferme le file descriptor de lecture  */
+        /* car on va juste écrire une donnée       */
+
+        int x;
+        printf("Entrer un entier: ");
+        scanf("%d", &x);
+
+        if (write(fd[1], &x, sizeof(int)) == -1) {
+            return 3;
+        }
+
+        close(fd[1]);
+    }
+    else {
+        /* code du processus père */
+
+        close(fd[1]);
+        /* on ferme le fd d'écriture car */
+        /* on va juste lire une donnée   */
+
+        if (wait(NULL) == -1) {  /* attend le processus fils */
+            return 4;
+        }
+
+        int y;
+        read(fd[0], &y, sizeof(int)); /* y <- x */
+        close(fd[0]);
+
+        printf("Valeur lue: %d\n", y);
+    }
+
+    return 0;
+}
